c6f1187a2aebc83450a9cd5591336d6a7f72f885
lisy <lisy@std.uestc.edu.cn>
2024-04-07 16:23:42 +0800

    增加并行

--- build_gcc.sh
+++ build_gcc.sh
@@ -6,7 +6,7 @@ rm -rf gcc_build
 mkdir gcc_build
 pushd gcc_build
 ${DCOV_DIR}/gcc-9.4.0/configure --prefix=${DCOV_DIR}/gcc_install --enable-languages=c,c++ --disable-multilib
-make
+make -j$(nproc)
 make install
 popd
 pushd /usr/bin
