84b7d18b6bad00d9e876c718c98a1ebd23aa3b79
lisy <lisy@std.uestc.edu.cn>
2024-06-13 13:58:24 +0800

    使文件名更统一

---
+++ extra/plot_comet_tf.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+file_path = './run_comet_tf.log'
+
+c_covs=[]
+
+def get_cov_num(t:str):
+    return int(t.split('=')[1].strip())
+
+f = open(file_path, 'r')
+lines = f.readlines()
+for line in lines:
+    if 'coverage_c=' in line:
+        c_cov = get_cov_num(line)
+        c_covs.append(c_cov)
+
+# 绘制曲线图
+plt.figure(figsize=(10, 5))
+plt.plot(c_covs, label='C  Coverage')
+plt.xlabel('API Iteration')
+plt.ylabel('Basic Code Block Coverage')
+plt.title(f'Coverage Curve for COMET on TensorFlow')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+
+# 保存图像为 PNG 格式
+plt.savefig('coverage_comet_tf.png', format='png')
